a78e172f2e99e1aa42e5ee831a3a4cd717e9dc4a
Majid Iqbal <majidqbl1@gmail.com>
2024-04-02 07:31:29 +0000

    changesss in multiple files

--- main.cpp
+++ main.cpp
@@ -1,14 +1,33 @@
 #include "COVIDPriorityQueue.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 int main() {
     COVIDPriorityQueue queue;
     std::ifstream file("patients.txt");
-    std::string name, precondition;
-    int age;
+    std::string line;
 
-    while (file >> name >> age >> precondition) {
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string name, precondition;
+        int age;
+
+        std::string temp;
+        while (iss >> temp) {
+            if (name.empty()) {
+                name = temp;
+            } else {
+                std::istringstream validator(temp);
+                if (validator >> age) {
+                    break;
+                } else {
+                    name += " " + temp;
+                }
+            }
+        }
+        iss >> precondition;
         queue.push(Patient(name, age, precondition == "Yes"));
     }
 
@@ -18,10 +37,10 @@ int main() {
 
     while (!queue.empty() && availableVaccines > 0) {
         Patient patient = queue.top();
-        std::cout << patient.name << std::endl;
+        std::cout << patient.name << " ";
         queue.pop();
         --availableVaccines;
     }
-
+    std::cout << std::endl;
     return 0;
 }
